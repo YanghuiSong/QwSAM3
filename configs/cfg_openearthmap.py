a92eb1a57ef04a3f586c97bbd9d1ba32605e96f7
YanghuiSong <131155805+YanghuiSong@users.noreply.github.com>
2026-05-01 22:52:33 +0800

    Add files via upload

---
+++ configs/cfg_openearthmap.py
@@ -0,0 +1,101 @@
+_base_ = ['./base_config.py']
+
+# Dataset
+data_root = '/data/public/OpenEarthMap'
+dataset_type = 'OpenEarthMapDataset'
+
+class_names = [
+    'background', 'bareland', 'grass', 'pavement', 'road',
+    'tree', 'water', 'cropland', 'building'
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(448, 448), keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+
+model = dict(
+    type='CachedSAM3OpenSegmentor',
+    classname_path='./configs/cls_openearthmap.txt',
+    optimize_method='hybrid_strict',
+    prob_thd=0.02,
+    bg_idx=0,
+    confidence_threshold=0.5,
+    use_sem_seg=True,
+    use_presence_score=True,
+    use_transformer_decoder=True,
+    slide_stride=0,
+    slide_crop=0,
+    prompt_batch_size=15,
+    image_batch_size=4,
+    slot_batch_size=0,
+    auto_tune_slot_batch=False,
+    execution_mode='per_image',
+    mask_query_chunk_size=32,
+    slot_chunk_size=4,
+    max_cross_image_slots=15,
+    max_mask_tensor_mb=1024,
+    group_images_by_size=True,
+    shared_image_encoder_batch=True,
+    compile_model=False,
+    processor_resolution=1008,
+    inference_dtype='fp32',
+    image_encoder_dtype='bf16',
+    class_aggregation='max',
+    class_aggregation_topk=2,
+    class_aggregation_temperature=0.4,
+    class_aggregation_gem_power=6.0,
+    class_aggregation_consensus_beta=8.0,
+    class_aggregation_reliability_alpha=0.7,
+    class_aggregation_coverage_penalty=0.2,
+    class_aggregation_consensus_boost=0.3,
+    class_aggregation_support_ratio=0.7,
+    class_aggregation_boost_agreement_threshold=0.8,
+    class_aggregation_boost_max_coverage=0.3,
+    class_aggregation_keep_ratio=0.85,
+    class_aggregation_max_selected=0,
+    class_aggregation_second_boost_scale=0.18,
+    class_aggregation_second_suppress_scale=0.0,  
+    cache_text_embeddings=True,
+    enable_expanded_prompt=True,
+    expanded_prompt_pool_path='./prompt_pools/cfg_openearthmap_pgrf_max_expanded_prompt_pool_hybrid_strict.pkl',
+    semantic_instance_fusion='pgrf',
+)
+
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=4,
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='img_dir/val',
+        ann_dir='ann_dir/val',
+        reduce_zero_label=False,
+        pipeline=test_pipeline))
+
+test_evaluator = dict(
+    type='CombinedMetrics',
+    iou_metrics=['mIoU'],
+    iou_thresholds=[0.5, 0.75],
+    ignore_index=255
+)
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        reduce_zero_label=False,
+        data_prefix=dict(
+            img_path='img_dir/val',
+            seg_map_path='ann_dir/val'),
+        pipeline=test_pipeline))
+
+test_cfg = dict(type='TestLoop')
+
+work_dir = './work_dirs/cfg_openearthmap_pgrf_max'
